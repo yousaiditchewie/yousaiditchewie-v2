9028f9b688146decdc99d08a7c5582ae20218b8e
Jacob Allen <jedijake@me.com>
2019-08-13 22:19:33 -0500

    Add about section, add blog roll to home page

--- src/pages/index.md
+++ src/pages/index.md
@@ -3,7 +3,12 @@ templateKey: index-page
 title: yousaiditchewie | Jacob Allen — Musician, Software Engineer, Educator from Nashville, TN
 heading: yousaiditchewie | JACOB ALLEN
 subheading: Musician, Software Engineer, and Educator from Nashville, TN.
+heroImage:
+  alt: A close-up of a paper filter filled with ground coffee
+  image: /img/products-grid3.jpg
 ---
+
+## About Me
 
 Ever since I was 15, I dreamed of being a full-time musician. I moved to Los Angeles after college to pursue that dream and was lucky enough to acheive it. The dream turned out not to be as glorious as I had hoped it would be, so I learned how to code and became a software engineer.
 
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,30 +4,50 @@ import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
+import BlogRoll from '../components/BlogRoll';
 
 export const IndexPageTemplate = ({
   // image,
   heading,
   subheading,
   content,
-  contentComponent
+  contentComponent,
+  heroImage
   // mainpitch,
   // description,
   // intro,
 }) => {
   const PageContent = contentComponent || Content;
   return (
-    <div className="HomePage-container container">
-      <h1 className="HomePage-heading">{heading}</h1>
-      <h3 className="HomePage-subheading">{subheading}</h3>
-      <PageContent className="content" content={content} />
-    </div>
+    <>
+      <section className="Hero">
+        <div className="Hero-container container">
+          <div className="Hero-image">
+            <PreviewCompatibleImage imageInfo={heroImage} />
+          </div>
+          <h1 className="Hero-heading">{heading}</h1>
+          <h3 className="Hero-subheading">{subheading}</h3>
+        </div>
+      </section>
+      <section className="About">
+        <div className="About-container container">
+          <PageContent className="About-content" content={content} />
+        </div>
+      </section>
+      <section className="BlogRoll">
+        <div className="BlogRoll-container container">
+          <BlogRoll />
+        </div>
+      </section>
+    </>
   );
 };
 
 IndexPageTemplate.propTypes = {
   heading: PropTypes.string,
-  subheading: PropTypes.string
+  subheading: PropTypes.string,
+  heroImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
 };
 
 const IndexPage = ({ data }) => {
@@ -36,6 +56,7 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
+        heroImage={frontmatter.heroImage}
         heading={frontmatter.heading}
         subheading={frontmatter.subheading}
         contentComponent={HTMLContent}
@@ -58,6 +79,16 @@ export const pageQuery = graphql`
       frontmatter {
         heading
         subheading
+        heroImage {
+          alt
+          image {
+            childImageSharp {
+              fluid(maxWidth: 526, quality: 92) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
       }
     }
   }
