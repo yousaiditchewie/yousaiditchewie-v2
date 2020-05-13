919e3908976ba51045443963ca71a46907b67ab3
Jacob Allen <jedijake@me.com>
2020-05-13 12:18:56 -0500

    Optimize meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware');
 
 module.exports = {
   siteMetadata: {
-    title: 'yousaiditchewie | Jacob Allen',
+    title: 'Jacob Allen | @yousaiditchewie',
     description:
-      'Author of The Working Musician’s Playbook: The Step-By-Step Guide to Achieving a Fulfilling Career as a Freelance Musician.'
+      'Musician, software engineer, and educator from Nashville, Tennessee guiding freelance musicians through the ups and downs of the music industry.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -14,22 +14,22 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads'
-      }
+        name: 'uploads',
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages'
-      }
+        name: 'pages',
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images'
-      }
+        name: 'images',
+      },
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
@@ -40,8 +40,8 @@ module.exports = {
           {
             resolve: 'gatsby-remark-relative-images',
             options: {
-              name: 'uploads'
-            }
+              name: 'uploads',
+            },
           },
           {
             resolve: 'gatsby-remark-images',
@@ -49,57 +49,57 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048
-            }
+              maxWidth: 2048,
+            },
           },
           {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
-              destinationDir: 'static'
-            }
-          }
-        ]
-      }
+              destinationDir: 'static',
+            },
+          },
+        ],
+      },
     },
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
-      }
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
     },
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'] // applies purging only on the bulma css file
-      }
+        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+      },
     }, // must be after other CSS plugins
     {
       resolve: 'gatsby-plugin-mailchimp',
       options: {
         endpoint:
-          'https://me.us20.list-manage.com/subscribe/post?u=a6f354c5cb7da315a75e1b3d8&amp;id=ad32155102'
-      }
+          'https://me.us20.list-manage.com/subscribe/post?u=a6f354c5cb7da315a75e1b3d8&amp;id=ad32155102',
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-85710672-5'
-      }
+        trackingId: 'UA-85710672-5',
+      },
     },
-    'gatsby-plugin-netlify' // make sure to keep it last in the array
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  developMiddleware: app => {
+  developMiddleware: (app) => {
     app.use(
       '/.netlify/functions/',
       proxy({
         target: 'http://localhost:9000',
         pathRewrite: {
-          '/.netlify/functions/': ''
-        }
+          '/.netlify/functions/': '',
+        },
       })
     );
-  }
+  },
 };
